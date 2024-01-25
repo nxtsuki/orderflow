893e946c1eaa0295047504e3bc84c37a38e13ae1
nxtsuki <53132104+nxtsuki@users.noreply.github.com>
2024-01-25 23:44:10 +0100

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,22 +1,16 @@
 import { Inter } from 'next/font/google';
-import Head from 'next/head';
 import './globals.css';
 
 const inter = Inter({ subsets: ['latin'], variable: "--font-sans" });
 
 export const metadata = {
-  title: 'orderflow',
-  description: 'private orderflow website',
+  title: 'Snaps Vault',
+  description: 'Private website',
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Head>
-        <meta httpEquiv='cache-control' content='no-cache' />
-        <meta httpEquiv='expires' content='0' />
-        <meta httpEquiv='pragma' content='no-cache' />
-      </Head>
       <body className={`${inter.variable}`}>{children}</body>
     </html>
   )
