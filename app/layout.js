791ee69a47544f2c63d9b514d58434b9c2d6cd11
nxtsuki <53132104+nxtsuki@users.noreply.github.com>
2024-01-25 23:56:51 +0100

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,4 @@
 import { Inter } from 'next/font/google';
-import Head from 'next/head';
 import './globals.css';
 
 const inter = Inter({ subsets: ['latin'], variable: "--font-sans" });
@@ -12,11 +11,6 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Head>
-        <meta httpEquiv='cache-control' content='no-cache' />
-        <meta httpEquiv='expires' content='0' />
-        <meta httpEquiv='pragma' content='no-cache' />
-      </Head>
       <body className={`${inter.variable}`}>{children}</body>
     </html>
   )
