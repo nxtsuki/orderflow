eee821e477311b7219353951d212f21be7b92b57
nxtsuki <53132104+nxtsuki@users.noreply.github.com>
2024-01-25 21:52:55 +0100

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,25 +1,9 @@
-import fsPromises from 'fs/promises';
-import path from 'path'
 import Layout from "@/components/layout";
 import axios from 'axios';
 
 async function getPageData() {
-    const response = await axios.get("https://github.com/nxtsuki/orderflow/blob/main/data.json");
-    return [
-        {
-            title: "GC Absorption 1",
-            date: "01-03-2024",
-            cover: ["https://i.imgur.com/BEByMF0.png", "https://i.imgur.com/BEByMF0.png"],
-            messages: [
-                {
-                    "texts": ["ciao", "lezzo"] 
-                },
-                {
-                    "texts": ["mazzo", "ccecco"]
-                }
-            ]
-        }
-    ]
+    const response = await axios.get("https://orderflow.site/data.json");
+    return response.data
 }
 
 export default async function Landing() {
